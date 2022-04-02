1d2d8b57673b4b30785f3f613a32a6dd9a6d6090
Fazekas Tamás <tamas.fzks@gmail.com>
2022-04-02 11:29:15 +0200

    4. verzió

--- js_feladatok_3/feladat4.js
+++ js_feladatok_3/feladat4.js
@@ -1,17 +1,21 @@
-        document.getElementById('rendel').addEventListener('click', () => {
-        let nev = document.forms['kabel_rendelo']['nev'].value;
-        let jelszo = document.forms['kabel_rendelo']['jelszo'].value;
-        let cim = document.forms['kabel_rendelo']['cim'].value;
-        let varos = document.forms['kabel_rendelo']['varos'].value;
-        let orszag = document.forms['kabel_rendelo']['orszag'].value;
-        let iszam = document.forms['kabel_rendelo']['iszam'].value;
-        let gyarto = document.forms['kabel_rendelo']['gyarto'].value;
-        let tipus = document.forms['kabel_rendelo']['tipus'].value;
-        //let hossz = document.forms['kabel_rendelo']['hossz'].value;
-        //let szin = document.forms['kabel_rendelo']['szin'].value;
+    
+    //$('#megrendeles').on('click', Rendeles);
+
+    function Rendeles(){
+        let nev = $('[name = nev]').val();
+        let jelszo = $('[name = jelszo]').val();
+        let cim = $('[name = cim]').val();
+        let varos = $('[name = varos]').val();
+        let orszag = $('[name = orszag]').val();
+        let iszam = $('#iszam').val();
+        let gyarto = $('[name = gyarto]').val();
+        let feltetelek = $('#check').is(':checked');
+        let tipus = $('[name = tipus]').val();
+        let hossz = $('[name = hossz]').val();        
+        let szin = $('[name = szin]').val();
         let minden_kitoltve = true;
         if (nev === '') {
-            minden_kitoltve = false;
+            minden_kitoltve = false;       
         alert('A név kitöltése kötelező!')
         }
         if (jelszo === '') {
@@ -26,38 +30,48 @@
             minden_kitoltve = false;
         alert('A város kitöltése kötelező!')
         }
-        if (ország === '') {
+        if (orszag === '') {
             minden_kitoltve = false;
         alert('Az ország kiválasztása kötelező!')
         }
          if (iszam === '') {
             minden_kitoltve = false;
         alert('Az  irányítószám megadása kötelező!')
-                }
+        }
         if (gyarto === '') {
             minden_kitoltve = false;
         alert('A név kitöltése kötelező!')
-                }
+        }
+        if (feltetelek === false){
+            minden_kitoltve = false;
+        alert('Fogadja el a vásárlási feltételeket!')
+        }
         if (tipus === '') {
             minden_kitoltve = false;
-        alert('A típus megadása kötelező!')
-                }
-        // if (hossz === '') {
-        //     minden_kitoltve = false;
-        // alert('A név kitöltése kötelező!')
-        //         }
-        // if (szin === '') {
-        //     minden_kitoltve = false;
-        // alert('A név kitöltése kötelező!')
-        //         }
+        alert('A típus kiválasztása kötelező!')
+        }
+        if (hossz === '') {
+            minden_kitoltve = false;
+        alert('A hossz kiválasztása kötelező!')
+        }
+        if (szin === '') {
+            minden_kitoltve = false;
+        alert('A szín kiválasztása kötelező!')
+        }
         if (minden_kitoltve){
             let rendeles = {
-            //nev: nev,
-            //email: email,
+            nev: nev,
+            jelszo: jelszo,
+            cim: cim,
+            varos: varos,
+            orszag: orszag,
+            iszam: iszam,
             gyarto: gyarto,
-            //hossz: hossz,
-            //szin: szin,                
+            feltetelek: 'elfogadva',
+            tipus: tipus,
+            hossz: hossz,
+            szin: szin,                
         }
         console.log(rendeles); 
-    }
-});
+        }
+    };
